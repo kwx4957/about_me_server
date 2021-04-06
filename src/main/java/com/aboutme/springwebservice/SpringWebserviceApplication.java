10ff915e8571ad60a617d693856c774178d64fa5
dansam <33277725+kwx4957@users.noreply.github.com>
2021-04-07 00:23:48 +0900

    db

--- src/main/java/com/aboutme/springwebservice/SpringWebserviceApplication.java
+++ src/main/java/com/aboutme/springwebservice/SpringWebserviceApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class SpringWebserviceApplication {
 
     public static final String APPLICATION_LOCATIONS ="spring.config.import="
-            + "optional:file:/home/ec2-user/app/abapplication.yml ,"
-            + "optional:file:/home/ec2-user/app/config/real-application.yml";
+            + "optional:classpath:application.yml";
+          //  + "optional:file:/home/ec2-user/app/config/real-application.yml";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(SpringWebserviceApplication.class)
