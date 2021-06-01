06a8ed6175c72cf7448b76c5a540c0c6465f1b4d
junsix <junsix99@gmail.com>
2021-06-01 22:31:24 +0900

    swagger 접근 허용

--- src/main/java/com/aboutme/springwebservice/auth/naver/security/SecurityConfig.java
+++ src/main/java/com/aboutme/springwebservice/auth/naver/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.aboutme.springwebservice.auth.naver.security.filter.JwtTokenFilterCon
 import com.aboutme.springwebservice.auth.naver.security.service.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +16,21 @@ import java.util.WeakHashMap;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
+
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+            // other public endpoints of your API may be appended to this array
+    };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -33,5 +49,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/v1/**").authenticated();
 
         http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(AUTH_WHITELIST);
     }
 }
