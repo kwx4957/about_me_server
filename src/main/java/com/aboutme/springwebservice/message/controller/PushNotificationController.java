5a3892a12f9daef3cc9f8c3ee927b1febc9130b0
dansam <33277725+kwx4957@users.noreply.github.com>
2021-07-21 21:33:41 +0900

    알림기능 구현 수정v2(자동 알림 구현)

--- src/main/java/com/aboutme/springwebservice/message/controller/PushNotificationController.java
+++ src/main/java/com/aboutme/springwebservice/message/controller/PushNotificationController.java
@@ -3,6 +3,7 @@ package com.aboutme.springwebservice.message.controller;
 import com.aboutme.springwebservice.message.model.PushNotificationRequest;
 import com.aboutme.springwebservice.message.service.PushNotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +20,20 @@ public class PushNotificationController {
 //        return null;
 //    }
 
-    //@Scheduled(cron= )
      @PostMapping("/Message/push")
      public void  sendPushNotification(@RequestBody PushNotificationRequest request ) throws Exception {
          pushNotificationService.sendPushNotification(request);
          return;
         }
+
+        @Scheduled(cron = "0 0 08 * * ?")
+     public void sendAutoNotification(){
+        PushNotificationRequest request = new PushNotificationRequest();
+        request.setToken("chsac4qNy0vPjIZWzhPtrH:APA91bGIpM3-24VuDldkFdfU28lqW2LKuN_EBQvkhJwEBuzldr6zEmfDxsw3daf2yFRv09cJrVdAE9zTn2v9xnl-Nz5YDwP5M7lRPcYMi8ekmwsdLIOhWbJ2gd_O4oms8S7sdx8YBvik");
+        request.setTitle("오늘의나");
+        request.setTopic("global");
+        request.setMessage("오늘은 질문 해주시는거죠?");
+        pushNotificationService.sendPushNotification(request);
+     }
 
 }
