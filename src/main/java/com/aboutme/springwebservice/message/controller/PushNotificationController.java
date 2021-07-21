765b05089b23a8747390494e3acc0093d8a1e072
dansam <33277725+kwx4957@users.noreply.github.com>
2021-07-21 17:20:16 +0900

    알림기능 구현

--- src/main/java/com/aboutme/springwebservice/message/controller/PushNotificationController.java
+++ src/main/java/com/aboutme/springwebservice/message/controller/PushNotificationController.java
@@ -1,22 +1,32 @@
 package com.aboutme.springwebservice.message.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.aboutme.springwebservice.message.model.PushNotificationRequest;
+import com.aboutme.springwebservice.message.model.response.PushNotificationResponse;
+import com.aboutme.springwebservice.message.service.PushNotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 
 @RestController
 public class PushNotificationController {
-    /*@GetMapping("/Message/pushNotification")
-    public PushNotificationVO getPushNotification()
-    {
-        return null;
-    }
 
-    @GetMapping("/Message/PushNotificationList")
-    public ArrayList<PushNotificationVO> getPushNotificationList()
-    {
-        return null;
-    }
-*/
+    @Autowired
+    PushNotificationService pushNotificationService;
+//
+//    @GetMapping("/Message/PushNotificationList")
+//    public ArrayList<PushNotificationVO> getPushNotificationList()
+//    {
+//        return null;
+//    }
+
+    //@Scheduled(cron= )
+     @PostMapping("/Message/push")
+     public ResponseEntity  sendPushNotification(@RequestBody PushNotificationRequest request ) throws Exception {
+         pushNotificationService.sendPushNotificationToToken(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(),"http://3.36.188.237:8080/Board/dailyColors/1"), HttpStatus.OK);
+        }
+
 }
---
+++ src/main/java/com/aboutme/springwebservice/message/service/FCMService.java
@@ -0,0 +1,97 @@
+package com.aboutme.springwebservice.message.service;
+
+import com.aboutme.springwebservice.message.model.PushNotificationRequest;
+import com.google.firebase.messaging.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class FCMService {
+
+    private Logger logger = LoggerFactory.getLogger(FCMService.class);
+
+    public void sendMessage(Map<String, String> data, PushNotificationRequest request)throws InterruptedException, ExecutionException {
+        Message message = getPreconfiguredMessageWithData(data, request);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String jsonOutput = gson.toJson(message);
+        String response = sendAndGetResponse(message);
+        logger.info("Sent message with data. Topic: " + request.getTopic() + ", " + response+ " msg "+jsonOutput);
+    }
+
+    public void sendMessageCustomDataWithTopic(Map<String, String> data, PushNotificationRequest request)throws InterruptedException, ExecutionException {
+        Message message = getPreconfiguredMessageWithDataCustomWithTopic(data, request);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String jsonOutput = gson.toJson(message);
+        String response = sendAndGetResponse(message);
+        logger.info("Sent message with data. Topic: " + data.get("topic") + ", " + response+ " msg "+jsonOutput);
+    }
+
+
+//    public void sendMessageWithoutData(PushNotificationRequest request)throws InterruptedException, ExecutionException {
+//        Message message = getPreconfiguredMessageWithoutData(request);
+//        String response = sendAndGetResponse(message);
+//        logger.info("Sent message without data. Topic: " + request.getTopic() + ", " + response);
+//    }
+
+    public void sendMessageToToken(PushNotificationRequest request)throws InterruptedException, ExecutionException {
+        Message message = getPreconfiguredMessageToToken(request);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String jsonOutput = gson.toJson(message);
+        String response = sendAndGetResponse(message);
+        logger.info("Sent message to token. Device token: " + request.getToken() + ", " + response+ " msg "+jsonOutput);
+    }
+
+    private String sendAndGetResponse(Message message) throws InterruptedException, ExecutionException {
+        return FirebaseMessaging.getInstance().sendAsync(message).get();
+    }
+
+    private ApnsConfig getApnsConfig(String topic) {
+        return ApnsConfig.builder()
+                .setAps(Aps.builder().setCategory(topic).setThreadId(topic).build()).build();
+    }
+
+    private Message getPreconfiguredMessageToToken(PushNotificationRequest request) {
+        return getPreconfiguredMessageBuilder(request).setToken(request.getToken())
+                .build();
+    }
+
+    private Message getPreconfiguredMessageWithoutData(PushNotificationRequest request) {
+        return getPreconfiguredMessageBuilder(request).setTopic(request.getTopic())
+                .build();
+    }
+
+    private Message getPreconfiguredMessageWithData(Map<String, String> data, PushNotificationRequest request) {
+        return getPreconfiguredMessageBuilder(request).putAllData(data).setToken(request.getToken())
+                .build();
+    }
+
+    private Message.Builder getPreconfiguredMessageBuilder(PushNotificationRequest request) {
+        ApnsConfig apnsConfig = getApnsConfig(request.getTopic());
+        return Message.builder()
+                .setApnsConfig(apnsConfig).setNotification(
+                        new Notification(request.getTitle(), request.getMessage()));
+    }
+
+
+    private Message getPreconfiguredMessageWithDataCustomWithTopic(Map<String, String> data, PushNotificationRequest request) {
+        return getPreconfiguredMessageBuilderCustomDataWithTopic(data, request).putAllData(data).setTopic(request.getTopic())
+                .build();
+    }
+
+    private Message.Builder getPreconfiguredMessageBuilderCustomDataWithTopic(Map<String, String> data, PushNotificationRequest request) {
+        ApnsConfig apnsConfig = getApnsConfig(data.get(request.getTopic()));
+        return Message.builder()
+                .setApnsConfig(apnsConfig).setNotification(
+                        new Notification(data.get("title"), data.toString()));
+    }
+
+
+
+}
