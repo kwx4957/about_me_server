02e2f49b16d2a4cfd09caec069a6ef554691df54
dansam <33277725+kwx4957@users.noreply.github.com>
2021-07-22 10:48:10 +0900

    알림기능 구현 수정v3(자동 알림 수정)

--- src/main/java/com/aboutme/springwebservice/message/controller/PushNotificationController.java
+++ src/main/java/com/aboutme/springwebservice/message/controller/PushNotificationController.java
@@ -13,7 +13,7 @@ public class PushNotificationController {
 
     @Autowired
     PushNotificationService pushNotificationService;
-//
+
 //    @GetMapping("/Message/PushNotificationList")
 //    public ArrayList<PushNotificationVO> getPushNotificationList()
 //    {
@@ -26,8 +26,8 @@ public class PushNotificationController {
          return;
         }
 
-        @Scheduled(cron = "0 0 08 * * ?")
-     public void sendAutoNotification(){
+    @Scheduled(cron = "0 0 08 * * ?")
+    public void sendAutoNotification(){
         PushNotificationRequest request = new PushNotificationRequest();
         request.setToken("chsac4qNy0vPjIZWzhPtrH:APA91bGIpM3-24VuDldkFdfU28lqW2LKuN_EBQvkhJwEBuzldr6zEmfDxsw3daf2yFRv09cJrVdAE9zTn2v9xnl-Nz5YDwP5M7lRPcYMi8ekmwsdLIOhWbJ2gd_O4oms8S7sdx8YBvik");
         request.setTitle("오늘의나");
