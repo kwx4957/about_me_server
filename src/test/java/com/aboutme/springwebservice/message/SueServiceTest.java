c313c76b3bf201103852c961159609cc2db792e1
dansam <33277725+kwx4957@users.noreply.github.com>
2021-11-07 14:43:46 +0900

    오류 수정

--- src/test/java/com/aboutme/springwebservice/message/SueServiceTest.java
+++ src/test/java/com/aboutme/springwebservice/message/SueServiceTest.java
@@ -3,6 +3,7 @@ package com.aboutme.springwebservice.message;
 import com.aboutme.springwebservice.board.entity.QnACategoryLevel;
 import com.aboutme.springwebservice.board.repository.QnACategoryLevelRepository;
 import com.aboutme.springwebservice.domain.UserInfo;
+import com.aboutme.springwebservice.domain.UserProfile;
 import com.aboutme.springwebservice.entity.BasicResponse;
 import com.aboutme.springwebservice.entity.CommonResponse;
 import com.aboutme.springwebservice.message.entity.DefaultReasonList;
@@ -22,21 +23,5 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RunWith(SpringRunner.class)
 @Service
 public class SueServiceTest {
-
-    //@Autowired
-   // private MockMvc mvc;
-    private SueRepository sueRepository;
-    private QnACategoryLevelRepository qnACategoryLevelRepository;
-    @Test
-    public ResponseEntity<?extends BasicResponse> sue(@RequestBody SueVO vo) {
-
-        QnACategoryLevel qnACategoryLevel = qnACategoryLevelRepository.findById(vo.getTargetQuestionId())
-                                                                      .orElseThrow(()-> new IllegalArgumentException("해당 글이 존재하지 않습니다"));
-        //임시Id
-        UserInfo authorId = UserInfo.builder().seq(vo.getSuedUserId()).build();
-        UserVoc userVoc= UserVoc.builder().reasonId(new DefaultReasonList(vo.getSueReason())).authorId(authorId).questionId(qnACategoryLevel).build();
-        sueRepository.save(userVoc);
-        return  ResponseEntity.ok().body( new CommonResponse<>());
-    }
 
 }
